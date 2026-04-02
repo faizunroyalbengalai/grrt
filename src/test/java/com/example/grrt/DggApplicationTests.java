af13a3007a4d6ec51732c5881d454b5707108e23
faizunroyalbengalai <faizun.royalbengalai@gmail.com>
2026-04-02 17:23:37 +0600

    chore: bootstrap Spring Boot starter from Spring Initializr

---
+++ src/test/java/com/example/grrt/DggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grrt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
